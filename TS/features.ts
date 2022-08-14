cc27ea42b5380bf0e7bef8586b5a5abd6ce4df6a
Александр <81959083+pitbrest@users.noreply.github.com>
2022-08-14 20:09:49 +0300

    Update features.ts

--- TS/features.ts
+++ TS/features.ts
@@ -16,4 +16,24 @@ class Control<NodeType extends HTMLElement = HTMLElement> {
   }
 }
 
-export default Control;
+class Signal<ListenerType> {
+  private listeners: Array <(params: ListenerType) => void>;
+  
+  constructor() {
+    this.listeners = [];
+  }
+ 
+  add(listener: (params: ListenerType) => void) {
+    this.listeners.push(listener);
+  }
+
+  remove(listener: (params: ListenerType) => void) {
+    this.listeners = this.listeners.filter((elem) => elem !== listener);
+  }
+
+  emit(params: ListenerType) {
+    this.listeners.forEach((listener) => listener(params));
+  }
+}
+
+export  {Control, Signal};
